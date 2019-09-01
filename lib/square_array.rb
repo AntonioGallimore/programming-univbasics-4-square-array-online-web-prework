de564daaefc5cd5d19e33c40df4efe5b58cfbb68
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-01 14:00:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,9 @@
 def square_array(array)
-  numbers = [1,2,3]
-  return square_array(array)
+   counter = 0
+  square_array = []
+  while array[counter] do
+    square_array.push(array[counter]**2)
+    counter += 1
+  end
+  square_array
 end
